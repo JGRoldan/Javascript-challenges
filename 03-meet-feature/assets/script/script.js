39324bbc5558c989960c3a20b0baf0fbd48c127e
Gaston Roldan <71336562+JGRoldan@users.noreply.github.com>
2023-03-13 22:54:25 -0300

    Update script.js

--- 03-meet-feature/assets/script/script.js
+++ 03-meet-feature/assets/script/script.js
@@ -1,18 +1,24 @@
-const icon = document.querySelector('.icon'),
-    items = document.querySelectorAll('ul li')
-    
+const items = document.querySelectorAll('ul li'),
+    body = document.querySelector('.body')
 
 items.forEach((item) => {
 
     item.addEventListener('click', (e) => {
-        icon.innerHTML = e.target.textContent
+
+        const icon = document.createElement('div')
+        icon.classList.add('icon')
         
+        icon.innerHTML = `
+            ${e.target.textContent}
+            <p>JGRDeveloper</p>
+        `
         icon.classList.add('slide-up')
+        icon.style.left = `${Math.random() * 50}%` 
+        body.appendChild(icon)
+
         setTimeout(() => {
             icon.classList.remove('slide-up')
+            body.removeChild(icon)
         },800)
     })
 })
-
-
-
